0c21cde9dccb3c98c2fe33f905e224c1e67d3e7c
zmeeks <zmeeks@users.noreply.github.com>
2017-08-29 16:30:56 -0700

    Delete leet_389b.cpp

--- leet_389/leet_389b.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    char findTheDifference(string s, string t) {
-        int ltr=0, len_s=s.size(), len_t=t.size();
-        for(int i=0; i<len_s; i++) ltr^=s[i];
-        for(int i=0; i<len_t; i++) ltr^=t[i];
-        return (char)ltr;                         
-    }
-};
