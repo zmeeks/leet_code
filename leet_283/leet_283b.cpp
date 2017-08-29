32fa90e8b97fad4eb31092dce7a4ed6c58c5f9ac
zmeeks <zmeeks@users.noreply.github.com>
2017-08-29 16:35:43 -0700

    Delete leet_283b.cpp

--- leet_283/leet_283b.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
-        reverse(nums.begin(),nums.end());
-        stable_sort(nums.begin(), nums.end(), [](int a, int b){return a!=0 || b==0;});
-    }
-};
-// same speed as first implementation but more concise code
